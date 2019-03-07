aaa23bb100dd4420fb5da5fbc5328b83010bc8e4
maneletorres <maneletorres@hotmail.com>
2019-03-07 18:09:42 +0100

    Improvement in the presentation and efficiency of the code of the functionality offered by the class 'JInternalFrame1'.

--- src/Vista/JInternalFrame1.java
+++ src/Vista/JInternalFrame1.java
@@ -2,18 +2,19 @@ package Vista;
 
 import Entitat.Pet;
 import Model.PetTableModel;
+import java.awt.HeadlessException;
 import java.awt.Image;
-import java.beans.PropertyVetoException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
 import javax.swing.JOptionPane;
-import javax.swing.event.InternalFrameAdapter;
-import javax.swing.event.InternalFrameEvent;
 
 /**
  *
@@ -24,11 +25,17 @@ public class JInternalFrame1 extends JInternalFrame {
     public JInternalFrame1() {
         initComponents();
 
+        try {
+            // idJTextField.setText(new PetBLL().maxCodi());
+            idJTextField.setText(new PetTableModel().getMaxCodi());
+        } catch (Exception ex) {
+            Logger.getLogger(JInternalFrame1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
         super.setVisible(true);
         super.setClosable(true);
         super.setTitle("Alta Animal");
         super.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        isWindowThrown = false;
     }
 
     /**
@@ -58,6 +65,10 @@ public class JInternalFrame1 extends JInternalFrame {
         clientJButton = new javax.swing.JButton();
         jLabel4 = new javax.swing.JLabel();
         comentarisJTextField = new javax.swing.JTextField();
+        jLabel7 = new javax.swing.JLabel();
+        tractamentJTextField = new javax.swing.JTextField();
+        jLabel8 = new javax.swing.JLabel();
+        xipJTextField = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -87,6 +98,8 @@ public class JInternalFrame1 extends JInternalFrame {
 
         clientJTextField.setEnabled(false);
 
+        idJTextField.setEnabled(false);
+
         ImageIcon imageIcon = new ImageIcon("lib/eye-outline.png"); // load the image to a imageIcon
         Image image = imageIcon.getImage(); // transform it
         Image newimg = image.getScaledInstance(15, 15,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way
@@ -100,6 +113,10 @@ public class JInternalFrame1 extends JInternalFrame {
 
         jLabel4.setText("Comentaris");
 
+        jLabel7.setText("Tractament");
+
+        jLabel8.setText("Xip");
+
         javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
         jPanel4.setLayout(jPanel4Layout);
         jPanel4Layout.setHorizontalGroup(
@@ -112,18 +129,22 @@ public class JInternalFrame1 extends JInternalFrame {
                             .addComponent(jLabel3)
                             .addComponent(jLabel2)
                             .addComponent(jLabel1))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 36, Short.MAX_VALUE)
-                        .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(idJTextField, javax.swing.GroupLayout.DEFAULT_SIZE, 200, Short.MAX_VALUE)
-                            .addComponent(nomJTextField))
-                        .addGap(0, 31, Short.MAX_VALUE)
+                        .addGap(36, 36, 36)
+                        .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(nomJTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(idJTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 43, Short.MAX_VALUE)
                         .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel6)
-                            .addComponent(jLabel5))
+                            .addComponent(jLabel7)
+                            .addComponent(jLabel5)
+                            .addComponent(jLabel8))
                         .addGap(18, 18, 18)
                         .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(clientJTextField, javax.swing.GroupLayout.DEFAULT_SIZE, 103, Short.MAX_VALUE)
-                            .addComponent(naixementJTextField))
+                            .addComponent(xipJTextField, javax.swing.GroupLayout.DEFAULT_SIZE, 103, Short.MAX_VALUE)
+                            .addComponent(clientJTextField)
+                            .addComponent(naixementJTextField, javax.swing.GroupLayout.DEFAULT_SIZE, 103, Short.MAX_VALUE)
+                            .addComponent(tractamentJTextField))
                         .addGap(18, 18, 18)
                         .addComponent(clientJButton)
                         .addGap(235, 235, 235))
@@ -138,19 +159,22 @@ public class JInternalFrame1 extends JInternalFrame {
         jPanel4Layout.setVerticalGroup(
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel4Layout.createSequentialGroup()
-                .addGap(30, 30, 30)
+                .addGap(21, 21, 21)
                 .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(jPanel4Layout.createSequentialGroup()
-                        .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel1)
-                            .addComponent(idJTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(30, 30, 30)
-                        .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel2)
-                            .addComponent(nomJTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel5)
-                            .addComponent(clientJTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jLabel1)
+                        .addComponent(idJTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jLabel5)
+                        .addComponent(clientJTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addComponent(clientJButton, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(30, 30, 30)
+                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jLabel8)
+                        .addComponent(xipJTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jLabel2)
+                        .addComponent(nomJTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addGap(30, 30, 30)
                 .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
@@ -160,7 +184,9 @@ public class JInternalFrame1 extends JInternalFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 30, Short.MAX_VALUE)
                 .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4)
-                    .addComponent(comentarisJTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(comentarisJTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel7)
+                    .addComponent(tractamentJTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap())
         );
 
@@ -173,7 +199,7 @@ public class JInternalFrame1 extends JInternalFrame {
                 .addComponent(jPanel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel7Layout.createSequentialGroup()
-                .addContainerGap(272, Short.MAX_VALUE)
+                .addContainerGap(288, Short.MAX_VALUE)
                 .addComponent(acceptJButton)
                 .addGap(98, 98, 98)
                 .addComponent(cancelJButton)
@@ -184,11 +210,11 @@ public class JInternalFrame1 extends JInternalFrame {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel7Layout.createSequentialGroup()
                 .addGap(0, 0, 0)
                 .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
+                .addGap(10, 10, 10)
                 .addGroup(jPanel7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(acceptJButton)
                     .addComponent(cancelJButton))
-                .addGap(0, 21, Short.MAX_VALUE))
+                .addGap(0, 20, Short.MAX_VALUE))
         );
 
         jDesktopPane1.setLayer(jPanel7, javax.swing.JLayeredPane.DEFAULT_LAYER);
@@ -197,7 +223,7 @@ public class JInternalFrame1 extends JInternalFrame {
         jDesktopPane1.setLayout(jDesktopPane1Layout);
         jDesktopPane1Layout.setHorizontalGroup(
             jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 801, Short.MAX_VALUE)
+            .addGap(0, 817, Short.MAX_VALUE)
             .addGroup(jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addComponent(jPanel7, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
@@ -223,30 +249,49 @@ public class JInternalFrame1 extends JInternalFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void acceptJButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_acceptJButtonActionPerformed
-        if (idJTextField != null && !idJTextField.getText().equals("") && nomJTextField != null && !nomJTextField.getText().equals("")
+        if (idJTextField != null && !idJTextField.getText().equals("")
+                && nomJTextField != null && !nomJTextField.getText().equals("")
                 && especieJTextField != null && !especieJTextField.getText().equals("")
                 && comentarisJTextField != null && !comentarisJTextField.getText().equals("")
                 && clientJTextField != null && !clientJTextField.getText().equals("")
-                && naixementJTextField != null && !naixementJTextField.getText().equals("")) {
+                && xipJTextField != null && !xipJTextField.getText().equals("")
+                && naixementJTextField != null && !naixementJTextField.getText().equals("")
+                && tractamentJTextField != null && !tractamentJTextField.getText().equals("")) {
             Pet p = new Pet();
-            p.setXip(idJTextField.getText());
+            //p.setNum_id(idJTextField.getText());
             p.setNom(nomJTextField.getText());
             p.setEspecie(especieJTextField.getText());
             p.setComentaris(comentarisJTextField.getText());
             p.setCodi_id(clientJTextField.getText());
-            p.setTractament("parche");
+            p.setXip(xipJTextField.getText());
+            p.setTractament(tractamentJTextField.getText());
 
             try {
-                SimpleDateFormat format = new SimpleDateFormat("dd/mm/yyyy");
+                SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
                 Date date = format.parse(naixementJTextField.getText());
-                java.sql.Date sql = new java.sql.Date(date.getTime());
-                p.setData(sql);
+                java.sql.Date sqlData_naixement = new java.sql.Date(date.getTime());
+                p.setData_naixement(sqlData_naixement);
 
-                new PetTableModel("").addPet(p);
-                this.dispose();
+                java.sql.Date sqlDate = new java.sql.Date(System.currentTimeMillis());
+                p.setData(sqlDate);
+
+                // Inserció de la mascota a la base de dades:
+                // new PetBLL().insertPet(p);
+                new PetTableModel().addPet(p);
+
+                // Es reseteja el formulari per a l'alta de mascotes:
+                resetForm();
+
+                JOptionPane.showMessageDialog(this,
+                        "S'ha afegit la següent mascota: \n\n" + p.toString(), "Informació",
+                        JOptionPane.INFORMATION_MESSAGE);
             } catch (ParseException ex) {
                 JOptionPane.showMessageDialog(this,
                         "Has introduit una data de naixement en un format incorrecte [dd/mm/yyyy].", "Error",
+                        JOptionPane.ERROR_MESSAGE);
+            } catch (HeadlessException ex) {
+                JOptionPane.showMessageDialog(this,
+                        "S'ha produït un error durant la inserció de la mascota " + p.getNom() + ".", "Error",
                         JOptionPane.ERROR_MESSAGE);
             }
         } else {
@@ -261,32 +306,47 @@ public class JInternalFrame1 extends JInternalFrame {
     }//GEN-LAST:event_cancelJButtonActionPerformed
 
     private void clientJButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_clientJButtonActionPerformed
-        if (!isWindowThrown) {
-            JInternalFrame2 jInternalFrame = new JInternalFrame2(clientJTextField);
-            jInternalFrame.addInternalFrameListener(new InternalFrameAdapter() {
-                @Override
-                public void internalFrameClosed(InternalFrameEvent e) {
-                    isWindowThrown = false;
+        ClientJPanel panel = new ClientJPanel(clientJTextField);
+        JButton acceptClientJButton = panel.getAcceptClientJButton();
+
+        acceptClientJButton.addMouseListener(new java.awt.event.MouseAdapter() {
+            @Override
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                if (evt.getClickCount() == 1) {
+                    clientJDialog.dispose();
                 }
-            });
-            jDesktopPane1.add(jInternalFrame);
-
-            try {
-                // This fa referència a JInternalFrame1:
-                //this.setSelected(false);
-                jInternalFrame.setSelected(true);
-            } catch (PropertyVetoException ex) {
-                Logger.getLogger(JInternalFrame1.class.getName()).log(Level.SEVERE, null, ex);
             }
+        });
 
-            isWindowThrown = true;
-        } else {
-            JOptionPane.showMessageDialog(this,
-                    "No pots obrir una nova finestra si ja roman oberta altra.", "Informació",
-                    JOptionPane.INFORMATION_MESSAGE);
-        }
+        clientJDialog = new JDialog();
 
+        // Sentència que permet que la finestra llançada aparegui centrada:
+        clientJDialog.setLocation(FinestraPrincipal.getJDesktopPane1Width() / 2 - clientJDialog.getSize().width / 2, FinestraPrincipal.getJDesktopPane1Height() / 2 - clientJDialog.getSize().height / 2);
+
+        clientJDialog.setTitle("Seleccio Clients");
+        clientJDialog.setModal(true);
+        clientJDialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        clientJDialog.setContentPane(panel);
+        clientJDialog.setResizable(false);
+        clientJDialog.pack();
+        clientJDialog.setVisible(true);
     }//GEN-LAST:event_clientJButtonActionPerformed
+
+    public void resetForm() {
+        try {
+            // idJTextField.setText(new PetBLL().maxCodi());
+            idJTextField.setText(new PetTableModel().getMaxCodi());
+            nomJTextField.setText("");
+            especieJTextField.setText("");
+            comentarisJTextField.setText("");
+            clientJTextField.setText("");
+            xipJTextField.setText("");
+            naixementJTextField.setText("");
+            tractamentJTextField.setText("");
+        } catch (Exception ex) {
+            Logger.getLogger(JInternalFrame1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton acceptJButton;
@@ -303,10 +363,14 @@ public class JInternalFrame1 extends JInternalFrame {
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
     private javax.swing.JPanel jPanel4;
     private javax.swing.JPanel jPanel7;
     private javax.swing.JTextField naixementJTextField;
     private javax.swing.JTextField nomJTextField;
+    private javax.swing.JTextField tractamentJTextField;
+    private javax.swing.JTextField xipJTextField;
     // End of variables declaration//GEN-END:variables
-    private boolean isWindowThrown;
+    private JDialog clientJDialog;
 }
